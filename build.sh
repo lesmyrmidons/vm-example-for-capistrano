703009ff04e1126180f0633664ed5afa2d357ab1
Kevin ARBOUIN <lesmyrmidons@gmail.com>
2013-11-04 01:04:20 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 git submodule init
 
-vagrant-debian-wheezy-64/build.sh
+cd vagrant-debian-wheezy-64 && ./build.sh
 
-cp parameters.rb.dist parameters.rb
+cd .. && cp parameters.rb.dist parameters.rb
 
 vagrant up --provision
 
